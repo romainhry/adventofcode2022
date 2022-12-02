19682bfacad7c9459bedb591c9c97874b8b4ead1
Romain Henry <romainhry@gmail.com>
2022-12-02 23:04:49 +0100

    day2 part 2

--- day02/rock_scissors_paper.py
+++ day02/rock_scissors_paper.py
@@ -4,26 +4,20 @@ def main():
     current_python_path = os.path.dirname(os.path.abspath(__file__))
     filepath=os.path.join(current_python_path,'data.csv')
     df = pd.read_csv(filepath, header=None, sep=" ")
-    points = {"A":1, "B":2, "C":3}
-    equivalence = {"X":"A", "Y":"B", "Z":"C"}
+    draw_points = {"A":1, "B":2, "C":3}
+    win_points = {"A":2, "B":3, "C":1}
+    loose_points={"A":3, "B":1, "C":2}
+    equivalence = {"X":"loose", "Y":"draw", "Z":"win"}
     strategy_point = 0
     for _, hand in df.iterrows():
-        my_hand_points = points[equivalence[hand[1]]]
-        oponent_point = points[hand[0]]
-        if oponent_point == my_hand_points:
-            strategy_point += 3
-        elif oponent_point < my_hand_points:
-            if my_hand_points - oponent_point == 1:
-                strategy_point += 6
+        round_result = equivalence[hand[1]]
+        if round_result == "win":
+            strategy_point += win_points[hand[0]] + 6
+        elif round_result == "draw":
+            strategy_point += draw_points[hand[0]] + 3
         else:
-            if oponent_point - my_hand_points == 2:
-                strategy_point += 6
-        strategy_point += my_hand_points
-
+            strategy_point += loose_points[hand[0]] + 0
     print(strategy_point)
-        
-
-
 
 if __name__ == "__main__":
     main()
