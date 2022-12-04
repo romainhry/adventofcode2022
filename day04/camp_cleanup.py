6f4fe7d58e08048717b6c99194331189545ef69c
Romain Henry <romainhry@gmail.com>
2022-12-04 15:03:50 +0100

    part 2

--- day04/camp_cleanup.py
+++ day04/camp_cleanup.py
@@ -12,11 +12,24 @@ def isSectionsInPartnerSection(section, partner_section):
     return False
 
 
+def isSectionsPartlyInPartnerSection(section, partner_section):
+    if (
+        int(section[0]) >= int(partner_section[0])
+        and int(section[0]) <= int(partner_section[1])
+    ) or (
+        int(section[1]) <= int(partner_section[1])
+        and int(section[1]) >= int(partner_section[0])
+    ):
+        return True
+    return False
+
+
 def findOverlappingPairNumber(file: str):
     current_python_path = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(current_python_path, file)
     with open(filepath) as f:
-        overlappingPairResult = 0
+        fullOverlappingPairResult = 0
+        partlyOverlappingPairResult = 0
         for pair in f.readlines():
             elf_sections = pair.split(",")
             elf_bounds = elf_sections[0].split("-")
@@ -24,8 +37,12 @@ def findOverlappingPairNumber(file: str):
             if isSectionsInPartnerSection(
                 elf_bounds, partner_bounds
             ) or isSectionsInPartnerSection(partner_bounds, elf_bounds):
-                overlappingPairResult += 1
-    return overlappingPairResult
+                fullOverlappingPairResult += 1
+            if isSectionsPartlyInPartnerSection(
+                elf_bounds, partner_bounds
+            ) or isSectionsPartlyInPartnerSection(partner_bounds, elf_bounds):
+                partlyOverlappingPairResult += 1
+    return [fullOverlappingPairResult, partlyOverlappingPairResult]
 
 
 if __name__ == "__main__":
--- day04/test_camp_cleanup.py
+++ day04/test_camp_cleanup.py
@@ -1,9 +1,9 @@
-import camp_cleanup
+from day04 import camp_cleanup
 
 
 def test_findOverlappingPairNumber_testDataReturns2():
-    assert camp_cleanup.findOverlappingPairNumber("test_data.csv") == 2
+    assert camp_cleanup.findOverlappingPairNumber("test_data.csv") == [2, 4]
 
 
 def test_findOverlappingPairNumber_dataReturns528():
-    assert camp_cleanup.findOverlappingPairNumber("data.csv") == 528
+    assert camp_cleanup.findOverlappingPairNumber("data.csv") == [528, 881]
